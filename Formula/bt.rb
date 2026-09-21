8ab1c6b58ea708f35cb96dc20d1bafa47e1389b5
Chapman <touch65536@gmail.com>
2026-09-22 01:52:08 +0800

    Update bt to 0.1.6

--- Formula/bt.rb
+++ Formula/bt.rb
@@ -1,17 +1,17 @@
 class Bt < Formula
   desc "B站直播开播工具 — 命令行一键开播/下播"
   homepage "https://github.com/QwerProg/bili-tools"
-  version "0.1.5"
+  version "0.1.6"
   license "MIT"
 
   on_intel do
-    url "https://github.com/QwerProg/bili-tools/releases/download/v0.1.5/bt-x86_64-macos.zip"
-    sha256 "0f2e8d3ff538b740c61e7dafc7a252832601de67394cf2d292b12a7053a93ca7"
+    url "https://github.com/QwerProg/bili-tools/releases/download/v0.1.6/bt-x86_64-macos.zip"
+    sha256 "2c388eb06106bd0fc3fd262f5ccdb1cfb00d78a86149a9c87495afba967c3495"
   end
 
   on_arm do
-    url "https://github.com/QwerProg/bili-tools/releases/download/v0.1.5/bt-arm64-macos.zip"
-    sha256 "7f649c020f68c43e1ac3a8cd10cdb23e083f07a6b95ca313c5702cb1c626ff0f"
+    url "https://github.com/QwerProg/bili-tools/releases/download/v0.1.6/bt-arm64-macos.zip"
+    sha256 "a94b3f64fa0ca53e044253e9c0e21584af27bfc7b04b80dca8a1aecf506bdf77"
   end
 
   def install
